80705f8d8610797b820d9b501174a0c1ddb0b84c
cagdasdostum <cagdascalayir@outlook.com.tr>
2025-10-23 20:47:45 +0300

    reverse geocode eklendi.

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ app.get("/app/stops/", (req, res) => {
 });
 
 // durak id'sine göre bir yanıt döndürüyor.
-app.get("/app/stops/:id", (req, res) => {
+app.get("/app/stops/:id", async (req, res) => {
   const id = Number(req.params.id); // id'yi number'a çevirdik.
 
   if (isNaN(id)) {
-    res.status(400).json({ message: "Lütfen geçerli bir sayı girin." });
+    return res.status(400).json({ message: "Lütfen geçerli bir sayı girin." });
   }
   // json'daki DURAK_ID, bizim istek yolladığımız id'ye eşitse bunu buluyoruz.
   const durak = tasksJSON.find((d) => d.DURAK_ID === id);
@@ -33,10 +33,31 @@ app.get("/app/stops/:id", (req, res) => {
       .status(404)
       .json({ message: "Aranan ID'de durak bulunamadı...", arananID: id });
   } else {
+    let acikAdres = "Adres bilgisi alınamadı..";
+    try {
+      const lat = durak.ENLEM;
+      const lon = durak.BOYLAM;
+      const geoApiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`;
+
+      const response = await fetch(geoApiUrl, {
+        headers: {
+          "User-Agent": "ESHOT-Duraklari-API/1.0 (cagdas3515@proton.me)",
+        },
+      });
+
+      const geoData = await response.json();
+
+      if (geoData && geoData.display_name) {
+        acikAdres = geoData.display_name;
+      }
+    } catch (error) {
+      console.error("Nominatim API Hatası:", error.message);
+    }
+
     res.status(200).json({
-      message: `${req.params.id}. durak.`,
+      message: `${durak.DURAK_ADI} durağı.`,
       bekleyenYolcuSayisi: Math.round(Math.random() * 10), // bunu kendim uydurdum.
-      durak: durak,
+      acikAdres: acikAdres,
     });
   }
 });
